25021ff1dda4053fc9b65c38d337e16178c6b8a9
rigole <foplacide@gmail.com>
2024-02-10 12:30:24 +0100

    connexion to mongo database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const bodyParser = require("body-parser")
+const { MongoClient } = require('mongodb')
 const cors = require("cors")
 const app = express()
 
+const url = 'mongodb://localhost:27017/recipe'
+
+const client =  new MongoClient(url);
 
 app.use(bodyParser.json());
 
@@ -11,6 +15,21 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(cors())
+
+
+async function databaseConnexion(){
+    try {
+        await client.connect()
+        console.log('Connected to MongoDB')
+    } catch (error) {
+        console.log(error)
+    }finally{
+        await client.close()
+    }
+}
+
+databaseConnexion().catch(console.error)
+
 
 const port = process.env.PORT || 3000;
 
